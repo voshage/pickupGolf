d488af6e9d7dd831c0ab7497a8874ec3bc6bdd7a
Andrew Voshage <andrew.voshage@gmail.com>
2013-11-21 17:38:49 -0600

    Initialize App with Rails new

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+PickupSports::Application.config.secret_key_base = '86c99abbb6488b58962cf1940fee8d2d7c6963bacc3f0b62f1a45509ece28db1427dab7e8896d492618c7060e08ccb7e99c4c907d585c2c36fa61f79e6081f62'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+PickupSports::Application.config.session_store :cookie_store, key: '_pickupSports_session'
