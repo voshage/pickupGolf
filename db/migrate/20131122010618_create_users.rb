58535a6344b9a61f3799e0523e21c2b2d43dc394
Andrew Voshage <andrew.voshage@gmail.com>
2013-11-21 22:47:25 -0600

    create user and add rotues

--- db/migrate/20131122010618_create_users.rb
+++ db/migrate/20131122010618_create_users.rb
@@ -1,10 +1,16 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :name
+      t.string :first_name
+      t.string :last_name
+      t.string :profile_name
+      t.string :email
+
       t.string :content
 
       t.timestamps
     end
+
+    add_index :users, :email, :unique => true
   end
 end
