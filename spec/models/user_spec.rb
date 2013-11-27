3cfff445c6784a571d7effe487bacc49cbe184ba
Andrew Voshage <andrew.voshage@gmail.com>
2013-11-27 00:40:40 -0600

    add user tests for first_nmae, last_name, profile_name, email

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,15 +2,64 @@ require 'spec_helper'
 
 describe User do
 
-  before { @user = User.new(first_name: "Example User", email: "user@example.com") }
+  before { @user = User.new(first_name: "Example", last_name: "User", profile_name: "user", email: "user@example.com") }
 
   subject { @user }
 
   it { should respond_to(:first_name) }
   it { should respond_to(:email) }
 
-  describe "when first_name is not present" do
+  describe "first_name is not present" do
   	before { @user.first_name = "" }
   	it { should_not be_valid }
   end
+
+  describe "last_name is not present" do
+  	before { @user.last_name = "" }
+  	it { should_not be_valid }
+  end
+
+
+
+  describe "when profile_name" do
+
+	  describe "when is not present" do
+	  	before { @user.profile_name = "" }
+	  	it { should_not be_valid }
+	  end
+
+	  describe "is not present" do
+	  	before do
+	  		@newUser = User.new
+	  		@newUser.profile_name = ""
+	  		@newUser.save
+	  	end
+
+	  	subject { @newUser.errors[:profile_name] }
+
+	  	it { should_not be_empty }
+	  end
+
+	  describe "is not unquie" do
+	  	before do 
+	  		@user = User.create(first_name: "Example", last_name: "User", profile_name: "user", 
+	  			email: "user@example.com")
+	  		@user2 = User.new(first_name: "Example", last_name: "User2", profile_name: "user", 
+	  			email: "user@example.com")
+	  	end
+
+ 		  subject { @user2 } 
+
+	  	it { should_not be_valid }
+	  end
+
+	  describe "is not formated correctly" do
+	  	before { @user.profile_name = "Pop 34 X" }
+
+ 		  subject { @user } 
+
+	  	it { should_not be_valid }
+	  	it { should be_new_record }
+	  end
+	end
 end
